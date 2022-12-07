e114d750713cd73a0bccb3e58cb6f5df1d4d833d
Nikitha Chettiar <nikithauc@gmail.com>
2022-12-06 22:22:26 -0800

    user beta rolled up

--- packages/test/tests/index.ts
+++ packages/test/tests/index.ts
@@ -3,5 +3,8 @@ import { apiClient } from "./testClient";
 async function test(){
 
     const response = await apiClient.usersById("").notificationsById("").get();
-
+    console.log(response);
+    const s = response?.additionalData;
 }
+
+test().then();
